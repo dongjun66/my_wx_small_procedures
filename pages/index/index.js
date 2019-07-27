9993b0d4d81ba5aa248f735b67e50fc2b34a5729
Dawn Dawn <dawndawn@DowndeMacBook-Pro.local>
2019-07-27 13:38:33 +0800

    wx first push

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+Page({
+    data: {},
+    onLoad() {},
+    onUnload() {},
+    onShow() {},
+    onHide() {},
+    onReady() {},
+    navigateToClick() {
+        wx.navigateTo({
+            url: '/pages/user-choose/user-choose'
+        })
+    },
+
+    request() {
+
+    }
+
+    /**
+     * @param {String} 
+    */
+})
